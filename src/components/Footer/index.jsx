b8449fb236bf9cd72c94ecc7e5054fb9facede76
gabriel <gabrielscotio@gmail.com>
2024-02-29 14:58:51 -0300

    footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from './styles'
+
+
+export function Footer() {
+    return(
+        <Container>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="31" viewBox="0 0 26 31" fill="none">
+                <path d="M13 0.744263L25.9904 8.24426V23.2443L13 30.7443L0.00961876 23.2443V8.24426L13 0.744263Z" fill="#4D585E"/>
+                </svg>
+                <span>food explorer</span>
+            </div>
+
+            <p>© 2023 - Todos os direitos reservados.</p>
+        </Container>
+    )
+}
--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,14 +1,11 @@
+import { Footer } from '../../components/Footer/index.jsx'
 import { Container } from './styles.js'
-import { HeaderUser } from '../../components/HeaderUser/index.jsx'
-import { Logo } from '../../components/Logo/index.jsx'
-import { OrderButton } from '../../components/OrderButton/index.jsx'
-import { HeaderAdmin } from '../../components/HeaderAdmin/index.jsx'
   
 export default function App() {
 
   return(
     <Container>
-      <HeaderAdmin></HeaderAdmin>
+      <Footer></Footer>
     </Container>
   )
 }
