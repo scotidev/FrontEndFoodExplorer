3025720a138aba21093d8b6d61a8022195ba9475
gabrielscoti42 <gabrielscotio@gmail.com>
2025-08-05 08:37:19 -0300

    api prod dev

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const isProduction = import.meta.env.PROD;
+
+const baseURL = isProduction
+  ? "https://foodexplorer-api-z2el.onrender.com"
+  : "http://localhost:3333";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL,
   withCredentials: true,
 });
